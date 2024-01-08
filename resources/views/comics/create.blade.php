fac04cbd48c8d0b0db528a0e055b51028cc35725
MaxPredathor <max.gilli98@gmail.com>
2024-01-08 14:46:33 +0100

    added create and store

---
+++ resources/views/comics/create.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+
+@section('title', 'Single Product')
+
+@section('content')
+    @include('partials.jumbo')
+    <main class="bg-light">
+        <div class="container">
+            <form action="{{ route('comics.store') }}" method="post" class="">
+                @csrf
+                <div class="form-group w-100">
+                    <label for="exampleInputEmail1">Comic Title</label>
+                    <input type="text" class="form-control" name="title" aria-describedby="emailHelp" placeholder="Title">
+                </div>
+                <div class="form-group w-100">
+                    <label for="exampleInputPassword1">Comic Description</label>
+                    <input type="text" class="form-control" name="description" placeholder="Desc">
+                </div>
+                <div class="form-group w-100">
+                    <label for="exampleInputPassword1">Comic Price</label>
+                    <input type="text" class="form-control" name="price" placeholder="Price">
+                </div>
+                <div class="form-group w-100 mb-3">
+                    <label for="exampleInputPassword1">Comic Type</label>
+                    <input type="text" class="form-control" name="type" placeholder="Type">
+                </div>
+                <button type="submit" class="btn btn-primary ">Submit</button>
+            </form>
+        </div>
+
+    </main>
+
+@endsection
